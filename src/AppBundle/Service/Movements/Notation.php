e774565068c03f8e17665eb4acb6398397014673
lucaMayer <luca.mayer-dalverny@isen.yncrea.fr>
2018-05-15 16:08:13 +0200

    Ajout de la classe Notation

---
+++ src/AppBundle/Service/Movements/Notation.php
@@ -0,0 +1,24 @@
+<?php
+namespace Service\Movements;
+
+abstract class Notation
+{
+    const CAPTURE = 'x';
+    
+    const CHECK = '+';
+    
+    const MATE = '#';
+    
+    const PAWN = '';
+    
+    const KNIGHT = 'N';
+    
+    const BISHOP = 'B';
+    
+    const ROOK = 'R';
+    
+    const QUEEN = 'Q';
+    
+    const KING = 'K';
+}
+
