0a9cacb2e094068904cbb42ba7d2c490e4216c08
lucaMayer <luca.mayer-dalverny@isen.yncrea.fr>
2018-05-30 11:18:14 +0200

    modification de l'affichage d'un plateau dans show.html.twig

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Game;
+use AppBundle\Service\Board\Board;
 
 /**
  * @Route("/game")
@@ -21,7 +23,7 @@ class GameController extends Controller
     }
 
     /**
-     * @Route("/remove")
+     * @Route("/remove/{id}", requirements={"id": "\d+"}, name="app_game_remove")
      */
     public function removeAction()
     {
@@ -31,12 +33,16 @@ class GameController extends Controller
     }
 
     /**
-     * @Route("/")
+     * @Route("/{id}", requirements={"id": "\d+"}, name="app_game_show")
      */
-    public function showAction()
+    public function showAction(int $id)
     {
+        $game = $this->getDoctrine()->getRepository(Game::class)->find($id);
+        
+        $board = new Board(false);
+        
         return $this->render('AppBundle:Game:show.html.twig', array(
-            // ...
+            'board' => $board->updateFromString($game->getBoard())
         ));
     }
 
