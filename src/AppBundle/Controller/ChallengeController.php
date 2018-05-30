0c6bd4c49a773fbdb02dc340d7c82a5277bf93e0
Anne <anne.sarrazin@isen.yncrea.fr>
2018-05-30 20:47:13 +0200

    Lancement de défi terminé

--- src/AppBundle/Controller/ChallengeController.php
+++ src/AppBundle/Controller/ChallengeController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Game;
+use AppBundle\Service\Board\Board;
+use phpDocumentor\Reflection\Types\Integer;
 
 class ChallengeController extends Controller
 {
@@ -25,7 +28,44 @@ class ChallengeController extends Controller
      * @Route("/createChallenge",name="createChallenge")
      * 
      */
-    public function createChallengeAction(){
+    public function createChallengeAction(Request $request){
+        if($request->isMethod('post')){
+            $game = new Game();
+            
+            $adId =intval($request->get('_opponent'));
+            $opponent = $this->getDoctrine()->getRepository(User::class)->findOneById($adId);
+            
+            
+            if($request->get('_color')==1){
+                $game->setWhitePlayer($this->getUser());
+                $game->setBlackPlayer($opponent);
+            }
+            else{
+                $game->setBlackPlayer($this->getUser());
+                $game->setWhitePlayer($opponent);
+            }
+           
+            
+            $game->setBoard("");
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($game);
+            $em->flush();
+           
+           
+            return $this->render('AppBundle:Challenge:challengeCreate.html.twig', array(
+                'valid' => 1 ,
+                'opponent' => $opponent->getUsername(),
+                'gameId' => $game->getId()
+            ));
+        }
+        
+        else{
+            return $this->render('AppBundle:Challenge:challengeCreate.html.twig', array(
+                'valid' => 0
+            ));
+            
+        }
         
     }
 
